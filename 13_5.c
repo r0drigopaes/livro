fd63e9831ec8607ce7d9b8b9102574e5ec70364b
Rodrigo Paes <rodrigobpaes@gmail.com>
2016-04-06 10:36:00 -0300

    corrigindo o identificador da estrutura

--- 13_5.c
+++ 13_5.c
@@ -2,7 +2,7 @@ void imprimir(struct cliente c)
 {
 	printf("Codigo.......: %d\n", c.codigo);
 	printf("Nome.........: %s", c.nome);
-	printf("Nascimento...: %d/%d/%d\n", c.dia_nascimento, c.mes_nascimento,	ano_nascimento);
+	printf("Nascimento...: %d/%d/%d\n", c.dia_nascimento, c.mes_nascimento,	c.ano_nascimento);
  	printf("Sexo.........: ");
 	if (c.sexo==MASCULINO)
 	{
